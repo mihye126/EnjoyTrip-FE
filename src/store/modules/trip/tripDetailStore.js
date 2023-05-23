71e9b02b4c3bf2ac2122f151ad3359a6460c50a5
chech2 <90683516+chech2@users.noreply.github.com>
2023-05-23 17:10:06 +0900

    [ADD] noticelist noticeitem

--- src/store/modules/trip/tripDetailStore.js
+++ src/store/modules/trip/tripDetailStore.js
@@ -14,7 +14,6 @@ export const tripDetailStore = {
   },
   mutations: {
     SET_TRIP_DETAIL: (state, data) => {
-      // console.log("이러지마 제발...", data);
       state.attraction = data;
       // console.log("확인 ", state.attraction);
     },
@@ -25,7 +24,6 @@ export const tripDetailStore = {
       await tripDetail(
         contentID,
         ({ data }) => {
-          // console.log(data);
           if (data.data != null) {
             commit("SET_TRIP_DETAIL", data.data);
           } else {
