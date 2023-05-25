7aaf61b551293dc6f36883d0ab37db98dc4638df
chech2 <90683516+chech2@users.noreply.github.com>
2023-05-25 14:14:38 +0900

    [ADD]trip_serch_page

---
+++ src/views/TripSerchView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <navigation-header></navigation-header>
+    <TripSearch></TripSearch>
+    <TripSerchList></TripSerchList>
+    <navigation-footer></navigation-footer>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TripSearch from "@/components/trip/TripSearch.vue";
+import TripSerchList from "@/components/trip/trip-serch/TripSerchList.vue";
+import NavigationFooter from "@/components/navigation/NavigationFooter.vue";
+import NavigationHeader from "@/components/navigation/NavigationHeader.vue";
+
+export default {
+  name: "TripSearchView",
+  components: {
+    TripSearch,
+    NavigationFooter,
+    NavigationHeader,
+    TripSerchList,
+  },
+};
+</script>
